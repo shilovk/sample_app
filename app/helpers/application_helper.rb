3f204667cbe141764a2a44698c76add94755fb4e
shilovk <shilovk@gmail.com>
2014-05-26 22:03:06 +0000

    Finish 5 chapter - tests upgraded

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-# Return the full title on a per-page basis.
+	# Return the full title on a per-page basis.
 
 	def full_title(page_title)
 		base_title = "Ruby on Rails Tutorial Sample App"
