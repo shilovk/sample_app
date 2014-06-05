49b07fac8b483b4f15257b51db33cc86eece1ab4
shilovk <shilovk@gmail.com>
2014-06-05 20:57:57 +0000

    Edit profile

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -33,3 +33,17 @@ def fill_in_example
   fill_in "Password",     with: "foobar"
   fill_in "Confirmation", with: "foobar"
 end
+
+def sign_in(user, options={})
+	if options[:no_capybara]
+		# Sign in when not using Capybara
+		remember_token = User.new_remember_token
+		cookies[:remember_token] = remember_token
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
+	else
+		visit signin_path
+		fill_in "Email", 		with: user.email
+		fill_in "Password", with: user.password
+		click_button "Sign in"
+	end
+end
