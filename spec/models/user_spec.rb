784c68fb186a65e43a5f7c47ed778a03ec25428b
shilovk <shilovk@gmail.com>
2014-06-10 11:45:36 +0000

    dependent destroy

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ describe User do
 	before { @user = User.new(name: "Example User", email: "user@example.com",
                     password: "foobar", password_confirmation: "foobar") }
 
-
 	subject { @user }
   
   it { should respond_to(:name) }
@@ -123,6 +122,33 @@ describe User do
   describe "remember_token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank } #it { expect(@user.remember_token).not_to be_blank }
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do | micropost |
+        # expect do
+        #   Micropost.find(micropost)
+        # end.to raise_error(ActiveRecord::RecordNotFound)
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+    end
   end
 end
 
